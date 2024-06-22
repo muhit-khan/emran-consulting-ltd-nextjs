a20e5b3d045ba53c2ef19b31af0648a9a651fd3b
Muhit Khan <muhitkhan_bd@outlook.com>
2024-06-22 13:05:29 +0600

    chore: Update dependencies and site configuration

--- src/config/site.js
+++ src/config/site.js
@@ -1,6 +1,6 @@
 export function constructMetadata({
-  title = "Abdullah Agency ",
-  description = "Abdullah Agency is an open-source website.",
+  title = "Emran Consultancy",
+  description = "Developed by Muhit Khan",
   image = "/agency.PNG",
   icons = "/favicon.ico",
   noIndex = false,
@@ -25,7 +25,7 @@ export function constructMetadata({
       creator: "@KING_IN_JUNGLE",
     },
     icons,
-    metadataBase: new URL("https://agency-website-abdullah.vercel.app/"),
+    metadataBase: new URL("https://muhitkhan.tech"),
     themeColor: "#FFF",
     ...(noIndex && {
       robots: {
